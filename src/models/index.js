9945124b564b5c0222833a7d59d62ecee6654382
Yash Garde <ygarde@ucsd.edu>
2019-05-06 13:18:28 -0700

    plant and garden schemas added

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
 import User from './user';
+import Garden from './garden';
 
 const connectDb = () => {
   return mongoose.connect(process.env.DATABASE_URL);
 };
 
-const models = { User };
+const models = { User, Garden };
 
 export { connectDb };
 
---
+++ src/models/plant.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+/* all the data we are storing per plant is not final yet, so this is
+ what it is for now. */
+const plantSchema = new mongoose.Schema({
+    scientificName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    commonName: {
+        type: String,
+        required: true
+    },
+    plantImage: {
+        type: Buffer,        // image is stored as Base64 encoded string which is basically Binary, Buffer is best way to store
+        default: null
+    },
+    /* everything after this point is up in 
+     the air whether it needs to be here and what the default values and stuff will be */
+    wateringFrequency: {
+        type: Number,
+        required: true      
+    },
+    precipitation_max: {
+        type: Number,
+        default: null
+    },
+    precipitation_min: {
+        type: Number,
+        default: null
+    },
+    moisture_use: {
+        type: Number,
+        default: null
+    },
+    temperature_min: {
+        type: Number,
+        default: null
+    },
+    temperature_max: {
+        type: Number,
+        default: null
+    }
+
+});
+
+const Plant = mongoose.model('Plant', plantSchema);
+
+export default Plant;
