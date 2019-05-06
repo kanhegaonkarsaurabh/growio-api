37cc761e6def04b95055b69861c1daf5cffa76f2
Yash Garde <ygarde@ucsd.edu>
2019-05-06 13:23:20 -0700

    imported models in index

--- src/models/index.js
+++ src/models/index.js
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 
 import User from './user';
 import Garden from './garden';
+import Plant from './plant'
 
 const connectDb = () => {
   return mongoose.connect(process.env.DATABASE_URL);
 };
 
-const models = { User, Garden };
+const models = { User, Garden, Plant };
 
 export { connectDb };
 
