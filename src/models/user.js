231e064ea5d0d3fc166d265f7c7ac89c218b1692
Yash Garde <ygarde@ucsd.edu>
2019-05-06 20:09:48 -0700

    fixed the user model to match auth

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,34 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
+  _id: {          // We override the default mongo '_id' with google's uid for the user 
+    type: String
   },
+  email: {
+    type: String,
+    required: [true, 'email required'],
+    unique: [true, 'email already registered']
+  },
+  name: {
+    type: String, 
+    default: null
+  }
 });
 
 userSchema.statics.findByLogin = async function(login) {
+  // First check the db to find the user by email if he passes email as param
   let user = await this.findOne({
-    username: login,
+    email: login
   });
 
+  // If not, then check the db using his name as the param
   if (!user) {
-    user = await this.findOne({ email: login });
+    user = await this.findOne({ name: login });
   }
 
   return user;
 };
 
-userSchema.pre('remove', function(next) {
-  this.model('Message').deleteMany({ user: this._id }, next);
-});
 
 const User = mongoose.model('User', userSchema);
 
