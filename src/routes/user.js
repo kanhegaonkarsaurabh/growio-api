39498ad2f22a50ccf099fc539500f1af9530cc62
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-05-11 14:57:41 -0700

    fixed issues with auth in this branch

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,19 +5,15 @@ const router = Router();
 const getUsers = async (req, res) => {
   const users = await req.context.models.User.find();
   return res.send(users);
-}
+};
 
 const getUser = async (req, res) => {
-  const user = await req.context.models.User.findById(
-    req.params.userId,
-  );
+  const user = await req.context.models.User.findById(req.params.userId);
   return res.send(user);
-}
+};
 
-router.route('/')
-      .get(getUsers);
+router.route('/').get(getUsers);
 
-router.route('/:userId')
-      .get(getUser);
+router.route('/:userId').get(getUser);
 
 export default router;
