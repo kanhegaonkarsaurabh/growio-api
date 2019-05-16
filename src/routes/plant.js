6e46f7780125c9fc67dd2bea577e4d8d156c9b08
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-05-16 16:36:45 -0700

    promise and callback hell

--- src/routes/plant.js
+++ src/routes/plant.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { parse } from 'querystring';
 //import { request } from 'https';
-var request = require('request');
+import reqPromise from 'request-promise';
 
 const multer = require("multer");
 const cloudinary = require("cloudinary");
@@ -32,6 +32,32 @@ const sleep = (milliseconds) => {
 // POST request middleware parser
 const parser = multer({storage: storage});
 
+
+const identifyOptions = (encImage) => {
+    return {
+        method: 'POST',
+        url: 'https://plant.id/api/identify',
+        headers: { 'Content-Type': 'application/json' },
+        body: {
+            key: process.env.PLANTID_APIKEY,
+            images: [encImage]
+        },
+        json: true
+    }
+};
+
+const getSuggestionsOptions = (requestId) => {
+    return {
+        method: 'POST',
+        url: 'https://plant.id/api/check_identifications',
+        headers: { 'Content-Type': 'application/json' },
+        body: {
+            key: process.env.PLANTID_APIKEY,
+            ids: [parseInt(requestId)]
+        },
+        json: true
+    };
+}
 
 // route to identify the plant
 // router.post('/identify', parser.single("image"), (req, res) => {
@@ -94,74 +120,98 @@ const parser = multer({storage: storage});
   
 
 
-const checkPlantId = async (apiPlantId) => {
-    console.log('Currently working on request: ', apiPlantId);
-    let suggestionsList = null;
-    await sleep(5000);
-    while (true) {
-        console.log('this is working :)');
-        await sleep(5000);              // IMPORTANT LINE
-        const resp = await request({
-            method: 'POST',
-            url: 'https://plant.id/api/check_identifications',
-            headers: { 'Content-Type': 'application/json' },
-            body: {
-                key: process.env.PLANTID_APIKEY,
-                ids: [parseInt(apiPlantId)]
-            },
-            json: true
-        }, (err, res, body) => {
-            if (err) {
-                console.log('ERROR: checkPlantId() function request failed', err);
-            }
-            if (res.body.suggestions) {
-                if (res.body.suggestions.length > 0) {
-                    suggestionsList = res.body.suggestions;
-                    console.log('this', suggestionsList);
-                    return;
-                }
-            }
-        });
-        if (suggestionsList !== null) {
-            console.log('inside if',suggestionsList)
-            break;
-        }
-        console.log(suggestionsList);
-    }
+// const checkPlantId = async (apiPlantId) => {
+//     console.log('Currently working on request: ', apiPlantId);
+//     let suggestionsList = null;
+//     await sleep(5000);
+//     while (!suggestionsList) {
+//         console.log('this is working :)');
+//         await sleep(5000);              // IMPORTANT LINE
+//         const resp = await request({
+//             method: 'POST',
+//             url: 'https://plant.id/api/check_identifications',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: {
+//                 key: process.env.PLANTID_APIKEY,
+//                 ids: [parseInt(apiPlantId)]
+//             },
+//             json: true
+//         }, (err, res, body) => {
+//             if (err) {
+//                 console.log('ERROR: checkPlantId() function request failed', err);
+//             }
+//             if (res.body.suggestions) {
+//                 if (res.body.suggestions.length > 0) {
+//                     suggestionsList = res.body.suggestions;
+//                     console.log('this', suggestionsList);
+//                     return;
+//                 }
+//             }
+//         });
+//     console.log('loop is running');
+//     }
     
-}
+// }
 
-const plantId = async (encImage) => {
-    await request({
-        method: 'POST',
-        url: 'https://plant.id/api/identify',
-        headers: { 'Content-Type': 'application/json' },
-        body: {
-            key: process.env.PLANTID_APIKEY,
-            images: [encImage]
-        },
-        json: true
-    }, (err, res, body) => { 
-        if (err) {
-            console.log('ERROR: platnId() request failed', err);
-            return;
-        }
+// const plantId = async (encImage) => {
+//     await request({
+//         method: 'POST',
+//         url: 'https://plant.id/api/identify',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: {
+//             key: process.env.PLANTID_APIKEY,
+//             images: [encImage]
+//         },
+//         json: true
+//     }, (err, res, body) => { 
+//         if (err) {
+//             console.log('ERROR: platnId() request failed', err);
+//             return;
+//         }
 
-        // extract the plant ID
-        if (body && body.id) {
-            const apiPlantId = (body.id);
-            console.log('api id: ', apiPlantId);
-            checkPlantId(apiPlantId);
+//         // extract the plant ID
+//         if (body && body.id) {
+//             const apiPlantId = (body.id);
+//             console.log('api id: ', apiPlantId);
+//             checkPlantId(apiPlantId);
+//         }
+//     })
+// }
+
+const suggestionsHelper = (plantRequestId, count, callback) => {
+    reqPromise(getSuggestionsOptions(plantRequestId))
+    .then(body => {
+        console.log('Try ' + count, body);
+        if (body.length > 0 ) {        
+            if (body[0].suggestions.length > 0) { // atleast one suggestions is made
+                callback(body);
+                return;
+            }
         }
+        suggestionsHelper(plantRequestId, count += 1, callback);
     })
+    .catch('ERROR: getSuggestions request failed');
 }
 
 const identifyPlant = async (req, res) => {
     // extract the base64 encoded image 
     const image = req.body.image;
-   // console.log(req.body);
-    // send to the Plant.ID API
-    plantId(image);
+
+    // promise pattern requests
+    reqPromise(identifyOptions(image))
+        .then(async (body) => {
+            const plantRequestId = body.id;
+            console.log('IN_PROGRESS identify')
+            let count = 0
+            await sleep(5000);
+            suggestionsHelper(plantRequestId, count, function (body) {        // callback function here ends everything
+                console.log('SUCCESS: The suggestions are the following', body);
+                res.send({body});
+            });
+        })
+        .catch((err) => {
+            console.log('ERROR: plant identify request failed', err);
+        });
 }
 
 router.route('/identify')
