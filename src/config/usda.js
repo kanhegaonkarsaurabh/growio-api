0b4ca5a93631ce9c25b7ef8a9c3d60ad10dc4415
Yash Garde <ygarde@ucsd.edu>
2019-05-19 16:29:37 -0700

    callback added

--- src/config/usda.js
+++ src/config/usda.js
@@ -1,14 +1,15 @@
-const queryPlantDetails = () => {
+const queryPlantDetails = (sciName, callback) => {
   // Darya, Priyal, Grace fill codeeeeee
   var request = require('request');
-  request('https://plantsdb.xyz/search?Genus=Abutilon&Species=abutiloides', function(
+  request('https://plantsdb.xyz/search?Genus=Abutilon&Species=abutiloides', async function(
     error,
     response,
     body,
   ) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    callback(body);
   });
 };
 
--- src/routes/usda.js
+++ src/routes/usda.js
@@ -1,19 +1,11 @@
 import { Router } from 'express';
-//import { queryPlantDetails } from '../config/usda';
+import { queryPlantDetails } from '../config/usda';
 var request = require('request');
 
 const router = Router();
 
 router.get('/', async function(req, res) {
-  request('http://plantsdb.xyz/search?Genus=Abutilon&Species=abutiloides', function(
-    error,
-    response,
-    body,
-  ) {
-    if (error) {
-      console.log(error);
-    }
-    console.log(body);
+  const resp = await queryPlantDetails('lol', body => {
     res.json(body);
   });
 });
