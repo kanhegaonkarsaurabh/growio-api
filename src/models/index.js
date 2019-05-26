65b8bfd3b750d1de544c902f0ad6d2e01e988fe7
kanhegaonkarsaurabh <saurabh.kanhegaonkar@gmail.com>
2019-05-26 13:55:01 -0700

    Plant week (#14)
    * web-scraper written for usda website
    * cron-job for every monday at 00:00 setup
    * plant of the week works
    * package.json commit

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,13 @@ import User from './user';
 import Garden from './garden';
 import Plant from './plant';
 import PersonalPlant from './personalPlant';
+import PlantOfTheWeek from './plantOfTheWeek';
 
 const connectDb = () => {
   return mongoose.connect(process.env.DATABASE_URL);
 };
 
-const models = { User, Garden, Plant, PersonalPlant };
+const models = { User, Garden, Plant, PersonalPlant, PlantOfTheWeek };
 
 export { connectDb };
 
